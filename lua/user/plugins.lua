a4f932d4294b8c629b14687f8d8bd4235aa18d53
jogonzro <jmgonzalezro@gmail.com>
2022-03-11 01:12:16 +0100

    changed the error on jsonl, added wakatime

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
   
   -- VimWiki
   use 'vimwiki/vimwiki'
+  -- Wakatime
+ use 'wakatime/vim-wakatime'
+    
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.0.5/share/lua/5.1/?/init.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.0.5/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.0.5/lib/lua/5.1/?.so"
+local package_path_str = "/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/jmgonzalezro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -254,6 +254,11 @@ _G.packer_plugins = {
     path = "/home/jmgonzalezro/.local/share/nvim/site/pack/packer/start/vim-bbye",
     url = "https://github.com/moll/vim-bbye"
   },
+  ["vim-wakatime"] = {
+    loaded = true,
+    path = "/home/jmgonzalezro/.local/share/nvim/site/pack/packer/start/vim-wakatime",
+    url = "https://github.com/wakatime/vim-wakatime"
+  },
   vimwiki = {
     loaded = true,
     path = "/home/jmgonzalezro/.local/share/nvim/site/pack/packer/start/vimwiki",
@@ -272,5 +277,6 @@ if should_profile then save_profiles() end
 end)
 
 if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
   vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
