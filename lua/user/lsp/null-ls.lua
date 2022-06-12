91bc61a8bf78683fd3b3f9fc6c30e6124c5f60e4
jogonzro <jmgonzalezro@gmail.com>
2022-06-12 20:03:09 +0200

    modified some things and git blame

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,34 +1,20 @@
-local null_ls = require("null-ls")
-local api = vim.api
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	return
+end
 
-local no_really = {
-    method = null_ls.methods.DIAGNOSTICS,
-    filetypes = { "markdown", "text" },
-    generator = {
-        fn = function(params)
-            local diagnostics = {
-                null_ls.builtings.diagnostics
-            }
-            -- sources have access to a params object
-            -- containing info about the current file and editor state
-            for i, line in ipairs(params.content) do
-                local col, end_col = line:find("really")
-                if col and end_col then
-                    -- null-ls fills in undefined positions
-                    -- and converts source diagnostics into the required format
-                    table.insert(diagnostics, {
-                        row = i,
-                        col = col,
-                        end_col = end_col,
-                        source = "no-really",
-                        message = "Don't use 'really!'",
-                        severity = 2,
-                    })
-                end
-            end
-            return diagnostics
-        end,
-    },
-}
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.register(no_really)
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		-- formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.autopep8,
+		formatting.stylua,
+    -- diagnostics.flake8
+	},
+})
