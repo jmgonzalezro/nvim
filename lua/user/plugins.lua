6790e9656a6da66d1c047959e9bd221b8a6c5fc3
jogonzro <jmgonzalezro@gmail.com>
2022-06-13 09:59:40 +0200

    config mini

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,6 +103,9 @@ return packer.startup(function(use)
   -- Whichkey
   use "folke/which-key.nvim"
 
+  -- Mini
+  use "echasnovski/mini.nvim"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
