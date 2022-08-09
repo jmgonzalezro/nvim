bda5aabb184d31006982cdd619ad85bc5b989f08
jogonzro <jmgonzalezro@gmail.com>
2022-08-09 22:36:15 +0200

    added black instead autopep8

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,8 +12,8 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.autopep8,
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.autopep8,
 		formatting.stylua,
     -- diagnostics.flake8
 	},
