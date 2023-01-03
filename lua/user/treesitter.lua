e8374e49851cc323b24b6df2af9ab9c4cd393b18
jogonzro <jmgonzalezro@gmail.com>
2023-01-03 18:49:00 +0100

    fixed hotkeys for alpha

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -52,10 +52,10 @@ require('nvim-treesitter.configs').setup {
     swap = {
       enable = true,
       swap_next = {
-        ['<leader>a'] = '@parameter.inner',
+        -- ['<leader>a'] = '@parameter.inner',
       },
       swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
+        -- ['<leader>A'] = '@parameter.inner',
       },
     },
   },
