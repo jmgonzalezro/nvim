a3424d98cf31fd433917335c3b4d7a909c4ab07e
jogonzro <jmgonzalezro@gmail.com>
2023-03-10 12:29:50 +0100

    change gruvbox theme to neovim lua gruvbox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,26 @@
+-- setup must be called before loading the colorscheme
+-- just in case gruvbox is the main theme
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = true,
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = true,
+  transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
 vim.cmd [[
 try
-  colorscheme nightfox
+  colorscheme gruvbox
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
