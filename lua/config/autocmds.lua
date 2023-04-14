a719aa81f8e8b8bf9d852328c6b3bc1f7b592545
jogonzro <jmgonzalezro@gmail.com>
2023-04-14 22:05:23 +0200

    Whichkey and copilot configuration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
